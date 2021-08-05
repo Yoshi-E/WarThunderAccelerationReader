2d5ce571d5d2f287bd49291123d5eccb91f0c6bd
Yoshi-E <server19960714@yahoo.de>
2021-08-05 18:27:25 +0200

    Update detect.py

--- detect.py
+++ detect.py
@@ -65,6 +65,9 @@ DEBUG = False
 
 if not os.path.exists(HASHFOLDER):
     os.makedirs(HASHFOLDER)
+    
+if not os.path.exists("csv"):
+    os.makedirs("csv")
 
 for folder in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "err", "N"]:
     if not os.path.exists(os.path.join(HASHFOLDER, folder)):
